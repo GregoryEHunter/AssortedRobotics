832a8df5dbf269cc364ed4462886badea5c9b7ef
f0024tn <f0024tn@LoanerT1791.thayer.dartmouth.edu>
2018-10-28 19:44:59 -0400

    plotting script for problem 1

---
+++ HW5/visualize_data.py
@@ -0,0 +1,53 @@
+import json
+from pprint import pprint as pp
+from angles import degrees_to_radians
+
+import matplotlib.pyplot as plt
+from math import cos, sin, pi
+
+# robot.txt file format
+# {"robot": {
+#     "position": {
+#         "x": 32,
+#         "y": 12
+#     },
+#     "orientation": { 
+#         "yaw": 232
+#     },
+#     "scan": {
+#         "ranges": [1,2,3...]
+#     }
+# }}    
+
+# open file put into python object
+with open("robot.txt") as file:
+	data = json.load(file)
+
+# load data into variables
+robot_x = data["robot"]["position"]["x"]
+robot_y = data["robot"]["position"]["y"]
+robot_theta = data["robot"]["orientation"]["yaw"]
+
+scan_ranges = data["robot"]["scan"]["ranges"]
+
+# iterate through ranges and plot
+fig = plt.figure()
+ax = fig.add_subplot(111)
+
+# plot the robot
+ax.scatter(robot_x, robot_y, color='red', marker= 'x')
+
+# create data arrays
+x = []
+y = []
+
+for i in range(len(scan_ranges)):
+	if scan_ranges[i] >= 0.03:
+		x.append(robot_x + scan_ranges[i] * cos(robot_theta + degrees_to_radians(i)))
+		y.append(robot_y + scan_ranges[i] * sin(robot_theta + degrees_to_radians(i)))
+
+ax.scatter(x, y, color='blue', marker= '.')
+
+plt.show()
+
+
